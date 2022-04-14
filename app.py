3bf7284df4a9188e95dce1ecf5e2b559284b1fec
Robert Kammerer <r.kammerer@gmx.de>
2022-04-14 18:59:31 +0200

    Upload

---
+++ app.py
@@ -0,0 +1,138 @@
+###############################
+# This program lets you       #
+# - Create a dashboard        #
+# - Every dashboard page is  #
+# created in a separate file  #  
+###############################
+
+# Python libraries
+import streamlit as st
+import requests
+from bs4 import BeautifulSoup
+import numpy as np
+import pandas as pd
+from reader import feed
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+import time
+import datetime as dt
+import stat
+import re
+
+# User module files
+from lib.functions import *
+from lib.eda import *
+from lib.nlp import *
+
+def main():
+  
+    #############  
+    # Main page #
+    ############# 
+
+    st.set_page_config(page_title='Trustpilot Review Analyzer',  layout='wide', page_icon='chart_with_upwards_trend')  
+    st.title("Trustpilot Review Analyzer")
+
+    col1, col2 = st.columns(2)
+
+    # Get Company Name and return URL
+    with col1:
+        company_name=  st.text_input('Please insert the name of the company you want to analyse')
+
+    if len(company_name) != 0:
+        company_urls = get_company(company_name)
+
+        # Get URL Choice and get Basic informations
+        with col2:
+            user_url_choice = st.selectbox("Please choose the correct option:", company_urls)
+        urllist, logo, overall_rating, claimed, requestor = get_basics(user_url_choice)
+
+    
+        col3, col4, col5, col6 = st.columns(4)
+        with col3:
+            st.image(logo,width=150)
+
+        with col4:
+            st.metric("Overall Rating", overall_rating)
+
+        with col5:
+            st.metric("Verified Profile", claimed)
+        
+        with col6:
+            st.metric("Activity", requestor[:16])
+    
+        st.markdown("""---""")  
+
+
+        # Start workflow
+        scraped_df=decision_path(urllist, user_url_choice)
+
+        #Preprocess Data
+
+        final_df = preprocessing(scraped_df)
+
+
+        # Create Visualisations Menu
+        st.subheader('Customer reviews over time')
+
+        min_date=dt.date.today()-dt.timedelta(365)
+        max_date=dt.date.today()
+
+        colstart, colend, colunit = st.columns(3)
+
+        with colstart:
+            startdate=st.date_input("Startdate",value=min_date)
+
+        with colend:
+            enddate = st.date_input("Enddate", value=max_date)
+
+        with colunit:
+            axis_unit = st.selectbox("Axis unit", ['month','year'])
+
+        # Print Visualisation
+        col7, col8 =st.columns(2)
+        with col7:
+            histogram(startdate, enddate,axis_unit, final_df)
+        with col8:
+            lineplot(startdate, enddate,axis_unit, final_df)
+
+        st.markdown("""---""") 
+        # Print Wordclouds
+
+        st.subheader('Most frequent words in positive an negative feedbacks')
+
+        
+        wc_df = final_df.dropna()
+
+        try:
+            positive
+            if (len(positive)+len(negative)) != len(wc_df):
+                corppos = spacy_cleaner(positive)
+                corpneg = spacy_cleaner(negative)
+
+                colpos, colneg = st.columns(2)
+                with colpos:
+                    my_wordcloud(corppos, title='Most used words in positive reviews')
+                with colneg:
+                    my_wordcloud(corpneg,title = 'Most used words in negative reviews')
+            else:
+                pass
+        except NameError:
+            
+            positive=wc_df[wc_df['pos/neg']=="positive"]['text']
+            negative=wc_df[wc_df['pos/neg']=="negative"]['text']
+
+            corppos = spacy_cleaner(positive)
+            corpneg = spacy_cleaner(negative)
+
+            colpos, colneg = st.columns(2)
+
+            with colpos:
+                my_wordcloud(corppos, title='Most used words in positive reviews')
+
+            with colneg:
+                my_wordcloud(corpneg,title = 'Most used words in negative reviews')
+
+
+main()
